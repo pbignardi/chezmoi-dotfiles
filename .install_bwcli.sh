d35c417743c8e6b9d796982e5c178bec986bfb4e
Paolo Bignardi <paolo.bigno@gmail.com>
2025-07-31 22:47:15 +0200

    update: install bitwarden cli manually using hook

---
+++ .install_bwcli.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if type bw > /dev/null 2>&1; then
+    bw login --check > /dev/null 2>&1
+    if test "$?" -neq 0; then
+        bw login
+    fi
+    export BW_SESSION=$(bw unlock --raw)
+else
+    cd /tmp
+    if ! [[ -d ~/bin ]]; then
+        mkdir ~/bin
+    fi
+
+    if [[ $(uname -s) == "Linux" ]]; then
+        wget https://github.com/bitwarden/clients/releases/download/cli-v2025.7.0/bw-linux-2025.7.0.zip
+        unzip bw-linux-2025.7.0.zip -d .
+    elif [[ $(uname -s) == "Darwin" ]]; then
+        wget https://github.com/bitwarden/clients/releases/download/cli-v2025.7.0/bw-macos-2025.7.0.zip
+        unzip bw-macos-2025.7.0.zip -d .
+    fi
+
+    chmod +x bw
+    mv bw ~/bin/bw
+    export PATH=~/bin:$PATH
+    cd -
+fi
