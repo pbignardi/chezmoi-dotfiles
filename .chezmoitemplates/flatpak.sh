9b32bf3e58283a3e67eb4a79f36f5066517f05b1
Paolo Bignardi <paolo.bigno@gmail.com>
2025-07-31 17:25:12 +0200

    feat: defined toml data file with packages and fonts

--- .chezmoitemplates/flatpak.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure Flathub
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-# Install flatpak packages
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub com.github.ahrm.sioyek
